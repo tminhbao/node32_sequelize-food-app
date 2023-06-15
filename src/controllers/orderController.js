6c66213a5252324c5c1368f9bd64fdfcba8d5fe0
tminhbao <tminhbao2001@gmail.com>
2023-06-15 11:15:57 +0700

    feat: like restaurant

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -5,8 +5,9 @@ const models = initModels(sequelize);
 
 export const addOrder = async (req, res) => {
     try {
-
+        let { user_id, food_id, amount, code, arr_sub_id } = req.body;
+        console.log(req.body);
     } catch (error) {
-
+        res.status(500).json({ error: error })
     }
 }
