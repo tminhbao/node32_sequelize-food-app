841359a1bd5a6e8e4aaacd889de97f7f313353b4
tminhbao <tminhbao2001@gmail.com>
2023-06-15 17:18:10 +0700

    feat: add orders

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -6,7 +6,16 @@ const models = initModels(sequelize);
 export const addOrder = async (req, res) => {
     try {
         let { user_id, food_id, amount, code, arr_sub_id } = req.body;
-        console.log(req.body);
+        await models.order.create({
+            user_id,
+            food_id,
+            amount,
+            code,
+            arr_sub_id
+        })
+        res.status(200).json({
+            message: 'Add orders successfully'
+        })
     } catch (error) {
         res.status(500).json({ error: error })
     }
